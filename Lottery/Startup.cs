73b665acb7c0cbceaacbeaa47a704f9431621c4f
charles-lei <charles.lei@activenetwork.com>
2016-12-18 14:46:17 +0800

    Init

---
+++ Lottery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lottery.Startup))]
+namespace Lottery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
